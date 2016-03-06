6520189c4909543fa8f704cb953bb04fe6b1fdc4
Robert Clifford <rjc9008@rit.edu>
2016-03-06 13:30:29 -0500

    Update BROBOT.py

--- BROBOT.py
+++ BROBOT.py
@@ -1,5 +1,10 @@
 import pygame
+import RPi.GPIO as GPIO
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(23,GPIO.OUT)
+GPIO.output(23,GPIO.HIGH)
 pygame.mixer.init()
-pygame.mixer.music.load("sandstorm.wav)
+pygame.mixer.music.load("sandstorm.wav")
 while pygame.music.mixer.get_busy() == True:
   continue
+GPIO.output(23,GPIO.LOW)
